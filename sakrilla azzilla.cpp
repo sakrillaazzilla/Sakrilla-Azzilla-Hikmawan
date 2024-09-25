90c4339855d99a11796f7ca6df711674c0e06b0b
sakrillaazzilla <sakrillaazzillahkm@gmail.com>
2024-09-25 13:23:31 +0700

    Add files via upload

---
+++ sakrilla azzilla.cpp
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main()
+{
+	int panjang = 10;
+	int lebar = 7;
+	int hasil = panjang * lebar;
+	printf("luas dari panjang 10 cm dan lebar 7 cm adalah %d cm", hasil);
+	return 0;
+}
---
+++ sakrillaazzilla.cpp
@@ -0,0 +1,6 @@
+#include <stdio.h>
+int main()
+{
+	printf("NAMA:SAKRILLA AZZILLA HIKMAWAN NIM:2441090 PRODI:INFORMATIKA");
+	return 0;
+}
